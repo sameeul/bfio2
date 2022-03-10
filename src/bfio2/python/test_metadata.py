dddc7fb24a0e2a3fbe5621a8300149ea29963cec
sameeul <sameeul@gmail.com>
2022-03-10 08:37:11 -0500

    clean up extra files

--- src/bfio2/python/test_metadata.py
+++
@@ -1,7 +0,0 @@
-from cgi import test
-from bfio2.python.bioreader import OmeTiffReader
-import matplotlib.pyplot as plt
-img = OmeTiffReader("/home/samee/axle_dev/fl_test/r01_x10_y05_z08.ome.tif")
-print(img.get_xml_metadata())
-meta_data = dict(img.get_xml_metadata())
-print(meta_data)
